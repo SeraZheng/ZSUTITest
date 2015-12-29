8cc04a3c72781617d47ea086f28a11d54dbe3d06
sera <462970213@qq.com>
2015-12-29 15:35:53 +0800

    Support different devices
    Support different devices

--- ZSOpenInTestForUTI/AppDelegate.m
+++ ZSOpenInTestForUTI/AppDelegate.m
@@ -15,21 +15,80 @@
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSLog(@"%@", launchOptions);
+    
+    return YES;
+}
+
+#pragma mark -
+#pragma mark Image view
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_9_0
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
+{
+    UINavigationController *navigation = (UINavigationController *)application.keyWindow.rootViewController;
+    ViewController *displayController = (ViewController *)navigation.topViewController;
+    
+    [displayController.imageView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:url]]];
+    [displayController.label setText:sourceApplication];
+    
+    return YES;
+}
+
+#else
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options
 {
     UINavigationController *navigation = (UINavigationController *)application.keyWindow.rootViewController;
-    [navigation popToRootViewControllerAnimated:NO];
-    
     ViewController *displayController = (ViewController *)navigation.topViewController;
-    displayController.fileURL = url;
     
-    QLPreviewController *preview = [[QLPreviewController alloc] initWithNibName:nil bundle:nil];
-    preview.dataSource = displayController;
-    preview.delegate = displayController;
-    [preview setCurrentPreviewItemIndex:0];
+    [displayController.imageView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:url]]];
+    [displayController.label setText:[options objectForKey:UIApplicationOpenURLOptionsSourceApplicationKey]];
     
-    [navigation pushViewController:preview animated:YES];
     return YES;
 }
+#endif
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Quick Look
+
+//#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_9_0
+//- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
+//{
+//    UINavigationController *navigation = (UINavigationController *)application.keyWindow.rootViewController;
+//    [navigation popToRootViewControllerAnimated:NO];
+//    
+//    ViewController *displayController = (ViewController *)navigation.topViewController;
+//    displayController.fileURL = url;
+//    
+//    QLPreviewController *preview = [[QLPreviewController alloc] initWithNibName:nil bundle:nil];
+//    preview.dataSource = displayController;
+//    preview.delegate = displayController;
+//    [preview setCurrentPreviewItemIndex:0];
+//    
+//    [navigation pushViewController:preview animated:YES];
+//    return YES;
+//}
+//
+//#else
+//- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options
+//{
+//    UINavigationController *navigation = (UINavigationController *)application.keyWindow.rootViewController;
+//    [navigation popToRootViewControllerAnimated:NO];
+//    
+//    ViewController *displayController = (ViewController *)navigation.topViewController;
+//    displayController.fileURL = url;
+//    
+//    QLPreviewController *preview = [[QLPreviewController alloc] initWithNibName:nil bundle:nil];
+//    preview.dataSource = displayController;
+//    preview.delegate = displayController;
+//    [preview setCurrentPreviewItemIndex:0];
+//    
+//    [navigation pushViewController:preview animated:YES];
+//    return YES;
+//}
+//#endif
 
 @end
